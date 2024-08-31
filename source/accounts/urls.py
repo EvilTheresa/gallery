99f14f5fc16f2bf70b52f134da66e9e3722be336
helux <kled.master@gmail.com>
2024-08-31 14:15:33 +0600

    Вьюхи и шаблоны аккаунт

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
-from accounts.views import
-
-app_name = "accounts"
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name="login.html"), name="login"),
-    path('logout/', LogoutView.as_view(), name="logout")
-    ]
+    # Аутентификация
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    # Профиль пользователя
+    path('<int:pk>/profile/', ProfileView.as_view(), name='profile'),
+]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.db.models import Q
+from django.http import HttpResponseBadRequest
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.views import View
+from django.views.generic import CreateView, DetailView, UpdateView, ListView
+
+from accounts.form import RegisterForm
+
+
+class RegisterView(CreateView):
+    model = get_user_model()
+    template_name = 'templates/user_create.html'
+    form_class = RegisterForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:index')
+        return next_url
+
+
+class ProfileView(DetailView):
+    model = get_user_model()
+    template_name = "templates/profile.html"
+    context_object_name = "user_obj"
+
